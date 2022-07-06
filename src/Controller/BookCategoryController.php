94eaeaf63adf47d47db0cb16ecc95fdc5cfa623e
alexey-ringo <alexey.ringo@gmail.com>
2022-07-07 01:54:22 +0300

    first service BookService

---
+++ src/Controller/BookCategoryController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\BookCategoryService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BookCategoryController extends AbstractController
+{
+    public function __construct(private BookCategoryService $bookCategoryService)
+    {
+    }
+
+    #[Route('/api/v1/book/categories')]
+    public function categories(): Response
+    {
+        return $this->json($this->bookCategoryService->getRepositories());
+    }
+}
