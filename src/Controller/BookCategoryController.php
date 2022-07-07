c0d6f3eb1544d65dde4e94a4ac73aece2a95d5bf
alexey-ringo <alexey.ringo@gmail.com>
2022-07-08 02:27:35 +0300

    swager doc

--- src/Controller/BookCategoryController.php
+++ src/Controller/BookCategoryController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Service\BookCategoryService;
+use Nelmio\ApiDocBundle\Annotation\Model;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Model\BookCategoryListResponse;
+use OpenApi\Annotations as OA;
 
 class BookCategoryController extends AbstractController
 {
@@ -13,7 +16,14 @@ class BookCategoryController extends AbstractController
     {
     }
 
-    #[Route('/api/v1/book/categories')]
+    /**
+     * @OA\Response(
+     *     response=200,
+     *     description="Returns book categories",
+     *     @Model(type=BookCategoryListResponse::class)
+     * )
+     */
+    #[Route(path: '/api/v1/book/categories', methods: ['GET'])]
     public function categories(): Response
     {
         return $this->json($this->bookCategoryService->getCategories());
