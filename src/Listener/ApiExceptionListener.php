0f0c5e06e03699faf0ea628fc24a2c8d261d9871
alexey-ringo <alexey.ringo@gmail.com>
2022-07-17 01:58:02 +0300

    global exceptions handler service

---
+++ src/Listener/ApiExceptionListener.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Listener;
+
+use App\Model\ErrorResponse;
+use App\Service\ExceptionHandler\ExceptionMappingResolver;
+use App\Service\ExceptionHandler\ExceptionMapping;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class ApiExceptionListener
+{
+    public function __construct(
+        private ExceptionMappingResolver $resolver,
+        private LoggerInterface          $logger,
+        private SerializerInterface      $serializer
+    )
+    {
+    }
+
+    public function __invoke(ExceptionEvent $event): void
+    {
+        $throwable = $event->getThrowable();
+        $mapping = $this->resolver->resolve(get_class($throwable));
+        if ($mapping === null) {
+            $mapping = ExceptionMapping::fromCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        // Настройка логгирования в завимости от типа ошибки - начиная с 500 и выше или по переданному признаку
+        if ($mapping->getCode() >= Response::HTTP_INTERNAL_SERVER_ERROR || $mapping->isLoggable()) {
+            $this->logger->error($throwable->getMessage(), [
+                'trace' => $throwable->getTraceAsString(),
+                'previous' => null !== $throwable->getPrevious() ? $throwable->getPrevious()->getMessage() : ''
+            ]);
+        }
+
+        //Если сообщение нужно скрывать - то возвращаем стандартное для данного кода
+        $message = $mapping->isHidden() ? Response::$statusTexts[$mapping->getCode()] : $throwable->getMessage();
+        $data = $this->serializer->serialize(new ErrorResponse($message), JsonEncoder::FORMAT);
+        $response = new JsonResponse($data, $mapping->getCode(), [], true);
+
+        //Назначаем response клиенту
+        $event->setResponse($response);
+    }
+
+}
